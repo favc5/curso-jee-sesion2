bbe5fc71e7daae30346747b3bd7ef48cb8ff0219
Cristian Nunez <cristian.nunez@ibm.com>
2019-07-23 14:57:16 -0400

    Se creo la clase tipo con sus respectivos constructores

--- curso-jee-pkmn/src/cl/ibm/pkmn/Habilidad.java
+++ curso-jee-pkmn/src/cl/ibm/pkmn/Habilidad.java
@@ -2,4 +2,9 @@ package cl.ibm.pkmn;
 
 public class Habilidad extends Pokemon {
 
+	public Habilidad(String nombre, int energia, Habilidad habilidades, Tipo tipo) {
+		super(nombre, energia, habilidades, tipo);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- curso-jee-pkmn/src/cl/ibm/pkmn/Tipo.java
+++ curso-jee-pkmn/src/cl/ibm/pkmn/Tipo.java
@@ -2,4 +2,42 @@ package cl.ibm.pkmn;
 
 public class Tipo extends Pokemon {
 
+	protected String tipos;
+	private float factorDaño;
+	private String debilidad;
+	
+	public Tipo(String nombre, int energia, Habilidad habilidades, Tipo tipo, String tipos, float factorDaño, String debilidad) {
+		super(nombre, energia, habilidades, tipo);
+		this.tipos=tipos;
+		this.factorDaño=factorDaño;
+		this.debilidad= debilidad;
+		
+	}
+
+	public String getTipos() {
+		return tipos;
+	}
+
+	public void setTipos(String tipos) {
+		this.tipos = tipos;
+	}
+
+	public float getFactorDaño() {
+		return factorDaño;
+	}
+
+	public void setFactorDaño(float factorDaño) {
+		this.factorDaño = factorDaño;
+	}
+
+	public String getDebilidad() {
+		return debilidad;
+	}
+
+	public void setDebilidad(String debilidad) {
+		this.debilidad = debilidad;
+	}
+
+
+	
 }
