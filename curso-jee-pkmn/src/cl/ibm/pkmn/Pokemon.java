4bf78c40a01de6bc16db37a82479835ed0212e13
Cristian Nunez <cristian.nunez@ibm.com>
2019-07-23 14:26:25 -0400

    Se agregaron los constructores a la clase pokemon

--- curso-jee-pkmn/src/cl/ibm/pkmn/Pokemon.java
+++ curso-jee-pkmn/src/cl/ibm/pkmn/Pokemon.java
@@ -1,23 +1,82 @@
 package cl.ibm.pkmn;
 
 public class Pokemon implements Fabrica{
-
-<<<<<<< HEAD
+ 
+		private String nombre;
+		private int energia;
+		private Habilidad habilidades;
+		private Tipo tipo;
+		
 	
-	public pokemon
-	{
-		public String nombre;
-		public int energia;
-		public Habilidad habilidades;
-		public Tipo tipo;
+	public Pokemon(String nombre, int energia, Habilidad habilidades, Tipo tipo) {
+			super();
+			this.nombre = nombre;
+			this.energia = energia;
+			this.habilidades = habilidades;
+			this.tipo = tipo;
+		}
+
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public int getEnergia() {
+		return energia;
+	}
+
+
+
+	public void setEnergia() {
+		int vida= (int)(Math.floor(Math.random()*(150-80+1)+80));
+		this.energia = vida;
+	}
+
+
+
+	public Habilidad getHabilidades() {
+		return habilidades;
+	}
+
+
+
+	public void setHabilidades(Habilidad habilidades) {
+		this.habilidades = habilidades;
+	}
+
+
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+	public void crearcaterpie() {
 		
+	}
+	
+	public void crearsquirtle() {
 		
 	}
 
-=======
-	public Pokemon()
-	{
-		
+	public void crearcharmander() {
+	
 	}
->>>>>>> 4066b67350932826220ba004961c0f0ff24ea4f8
+	
 }
