601cd45c5bceaaf879b60ed2315fdb2095747594
Emanuel Cordova <emanuel.cordova@ibm.com>
2019-07-23 14:51:48 -0400

    Nueva Clase Habilidad

--- curso-jee-pkmn/src/cl/ibm/pkmn/Habilidad.java
+++ curso-jee-pkmn/src/cl/ibm/pkmn/Habilidad.java
@@ -1,5 +1,32 @@
 package cl.ibm.pkmn;
 
 public class Habilidad extends Pokemon {
+	
+	public String NombreHabilidad;
+	public int PuntosdeDaño[];
 
+	public Habilidad(String nombre, int energia, Habilidad habilidades, Tipo tipo, String nombreHabilidad,
+			int[] puntosdeDaño) {
+		super(nombre, energia, habilidades, tipo);
+		NombreHabilidad = nombreHabilidad;
+		PuntosdeDaño = puntosdeDaño;
+	}
+
+	public String getNombreHabilidad() {
+		return NombreHabilidad;
+	}
+
+	public void setNombreHabilidad(String nombreHabilidad) {
+		NombreHabilidad = nombreHabilidad;
+	}
+
+	public int[] getPuntosdeDaño() {
+		return PuntosdeDaño;
+	}
+
+	public void setPuntosdeDaño(int[] puntosdeDaño) {
+		PuntosdeDaño = puntosdeDaño;
+	}
+	
+	
 }
